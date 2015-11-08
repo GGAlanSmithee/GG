a9b83bcc3c3a3228c64c1b1ce11d4be6a91b83a2
Alan Smithee <ggnore.alan.smithee@gmail.com>
2015-11-08 15:36:12 +0000

    simplifies build pipe a lot by replacing rollup with browserify and babelify using babel 6

--- src/gg-100k.js
+++ src/gg-100k.js
@@ -9,7 +9,7 @@ import DI from './utility/dependency-injector';
 
 window.onload = function() {
     let game = new Game(new EntityManager(), DI.rendererManager());
-    
+        
     MainLoop.setUpdate(delta => { game.update(delta); })
             .setDraw(interpolationPercentage => { game.render(interpolationPercentage); })
             .start();
