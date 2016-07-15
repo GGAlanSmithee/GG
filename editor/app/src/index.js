418a1838648c9f6c1ecfe254275de3d052d8f96c
Alan Smithee <ggnore.alan.smithee@gmail.com>
2016-07-15 09:22:07 +0000

    removes unused ECS related code

--- editor/app/src/index.js
+++ editor/app/src/index.js
@@ -1,6 +1,6 @@
 import GG from 'gg'
 
-import velocity from '/home/ubuntu/workspace/editor/app/src/components/velocity.json'
+
 
 import collision, { Components as collisionComponents } from '/home/ubuntu/workspace/editor/app/src/systems/logic/collision.js'
 
@@ -8,7 +8,7 @@ import collision, { Components as collisionComponents } from '/home/ubuntu/works
 export default () => {
     const gg = new GG()
     
-    gg.entityManager.registerComponent('velocity', velocity)
+
 
 
 
